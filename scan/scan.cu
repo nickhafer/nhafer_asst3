840a48b5b2e51395fed4ccec54570919d9ce880d
nickhafer <107949449+nickhafer@users.noreply.github.com>
2024-11-05 21:13:50 -0800

    optimization test

--- scan/scan.cu
+++ scan/scan.cu
@@ -27,7 +27,8 @@ static inline int nextPow2(int n) {
     return n;
 }
 
-__global__ void upsweep_kernel(int two_d, int N, int* result) {
+__global__ void upsweep_kernel(int two_d, int N, int *result)
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int two_dplus1 = 2 * two_d;
 
@@ -37,7 +38,8 @@ __global__ void upsweep_kernel(int two_d, int N, int* result) {
     }
 }
 
-__global__ void downsweep_kernel(int two_d, int N, int* result) {
+__global__ void downsweep_kernel(int two_d, int N, int *result)
+{
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int two_dplus1 = 2 * two_d;
 
@@ -49,7 +51,8 @@ __global__ void downsweep_kernel(int two_d, int N, int* result) {
     }
 }
 
-__global__ void set_last_element(int *result, int N) {
+__global__ void set_last_element(int *result, int N)
+{
     result[N - 1] = 0;
 }
 
@@ -70,13 +73,13 @@ __global__ void set_last_element(int *result, int N) {
 // places it in result
 void exclusive_scan(int* input, int N, int* result)
 {
-    // Calculate the rounded length
     int rounded_length = nextPow2(N);
 
     // Upsweep phase
     for (int two_d = 1; two_d <= rounded_length / 2; two_d *= 2)
     {
-        int num_blocks = (rounded_length + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+        int num_threads = (rounded_length + 1) / 2; // Maximum number of threads needed
+        int num_blocks = (num_threads + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
         upsweep_kernel<<<num_blocks, THREADS_PER_BLOCK>>>(two_d, rounded_length, result);
         cudaDeviceSynchronize();
     }
@@ -88,7 +91,8 @@ void exclusive_scan(int* input, int N, int* result)
     // Downsweep phase
     for (int two_d = rounded_length / 2; two_d >= 1; two_d /= 2)
     {
-        int num_blocks = (rounded_length + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+        int num_threads = (rounded_length + 1) / 2; // Maximum number of threads needed
+        int num_blocks = (num_threads + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
         downsweep_kernel<<<num_blocks, THREADS_PER_BLOCK>>>(two_d, rounded_length, result);
         cudaDeviceSynchronize();
     }
